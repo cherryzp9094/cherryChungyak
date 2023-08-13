5f95ae64acd99e8c339caf8a348ef3ba9dfc355e
cherry <cherryzp9094@gmail.com>
2023-08-13 17:52:05 +0900

    chore: app / domain / data 레이어 project implementation 추가한다

--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -27,15 +27,16 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "17"
     }
 }
 
 dependencies {
+    implementation(project(":domain"))
 
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
