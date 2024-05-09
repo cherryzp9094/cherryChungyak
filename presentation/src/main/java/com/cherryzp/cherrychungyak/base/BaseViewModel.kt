fb2f5d9f2f2a748ebc8b4bc8606f94be90217d60
cherry <cherryzp9094@gmail.com>
2024-05-09 23:45:40 +0900

    feat: versionCatalog aac ViewModel 추가 및 BaseViewModel 생성한다

---
+++ presentation/src/main/java/com/cherryzp/cherrychungyak/base/BaseViewModel.kt
@@ -0,0 +1,9 @@
+package com.cherryzp.cherrychungyak.base
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+
+@HiltViewModel
+class BaseViewModel: ViewModel() {
+
+}
