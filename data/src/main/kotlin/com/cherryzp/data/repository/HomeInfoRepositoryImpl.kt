f0fe705576d37ba2c193e659331494b0756c9094
cherry <cherryzp9094@gmail.com>
2024-05-19 18:06:35 +0900

    feat: HomeAptInfoDetail mapper 추가 및 repository에 연결한다

--- data/src/main/kotlin/com/cherryzp/data/repository/HomeInfoRepositoryImpl.kt
+++ data/src/main/kotlin/com/cherryzp/data/repository/HomeInfoRepositoryImpl.kt
@@ -1,15 +1,18 @@
 package com.cherryzp.data.repository
 
+import com.cherryzp.data.mapper.toDomain
 import com.cherryzp.data.source.HomeInfoNetworkDataSource
 import com.cherryzp.domain.model.HomeAptInfoDetail
 import com.cherryzp.domain.repository.HomeInfoRepository
 import javax.inject.Inject
 
 class HomeInfoRepositoryImpl @Inject constructor(
-    private val dataSource: HomeInfoNetworkDataSource
-): HomeInfoRepository {
-    override suspend fun getHomeAptInfo(page: Int, perPage: Int): List<HomeAptInfoDetail> {
-        TODO("Not yet implemented")
-    }
+    private val dataSource: HomeInfoNetworkDataSource,
+) : HomeInfoRepository {
+    override suspend fun getHomeAptInfo(page: Int, perPage: Int): List<HomeAptInfoDetail> =
+        dataSource.getHomeAptInfo(page, perPage)
+            .map {
+                it.toDomain()
+            }
 
 }
